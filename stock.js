04f3599367f9b83250e9033ce67bd5a7079ed3b6
Estaless <alessandriest@gmail.com>
2022-12-28 23:16:33 -0300

    subiendo a git

---
+++ stock.js
@@ -0,0 +1,18 @@
+let stockProductos = [
+    {
+    id: 1, 
+    nombre: "Buzo 1",
+    tipo: "buzo",
+    cantidad: 1,
+    desc: "Te va a quedar perfecta",
+    precio: 1200,
+    talle: "L",
+    img: './img/buzo.jpg'},
+
+    {id: 2, nombre: "Buzo 2", tipo: "buzo", cantidad: 1, desc: "Te va a quedar perfecta", precio: 1100, talle: "L", img: './img/buzo.jpg'},
+    {id: 3, nombre: "Buzo 3", tipo: "buzo", cantidad: 1, desc: "Te va a quedar perfecta", precio: 1200, talle: "M", img: './img/buzo.jpg'},
+    {id: 4, nombre: "Buzo 4", tipo: "buzo", cantidad: 1, desc: "Te va a quedar perfecta", precio: 1400, talle: "M", img: './img/buzo.jpg'},
+    {id: 5, nombre: "Buzo 5", tipo: "buzo", cantidad: 1, desc: "Te va a quedar perfecta", precio: 1200, talle: "S", img: './img/buzo.jpg'},
+    {id: 6, nombre: "Buzo 6", tipo: "buzo", cantidad: 1, desc: "Te va a quedar perfecta", precio: 1500, talle: "S", img: './img/buzo.jpg'},
+    
+]
